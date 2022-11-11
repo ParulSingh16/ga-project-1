8ff2c856e84f870c48ab9de3c179ab71c2fb516f
ParulSingh16 <116558560+ParulSingh16@users.noreply.github.com>
2022-11-11 13:50:38 +0000

    basic html framework

---
+++ scripts/snake.js
@@ -0,0 +1,9 @@
+function init() {
+  const grid = document.querySelector(".grid");
+  const start = document.querySelector(".start");
+  const ScoreDisplay = document.querySelector("#score-display");
+
+  const lostGame = false;
+  const gridSize = 21;
+  const snakeSpeed = 5;
+}
