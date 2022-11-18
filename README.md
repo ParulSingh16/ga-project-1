08131fc665d9b5b413b19cd1fbba3b47e468a445
ParulSingh16 <116558560+ParulSingh16@users.noreply.github.com>
2022-11-18 10:05:20 +0000

    changes to refactor

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # ga-project-1
+
+**_Description_**
+"SNAKE GAME"
+
+The Snake Game here has been build by using the following:
+
+- JavaScript
+- HTML
+- CSS
+
+Here, give a short description of the project. It can be a couple of sentences where you discuss the point in time during the course that you completed it, the topic of the project and potentially the tech stack.
+
+**_About the Game_**
+
+I have build a simple and basic" Snake Game" here, which has a functionality of creating a "game-board", a "Snake" and his "food" as the main features.
+
+In this game everytime the snake eats the food the snake gets bigger.
+
+**_Playing Instructions_**
+
+To Start: press the "ENTER" key on the keyboard,
+To move Left: press the "LEFT" ARROW" key on the keyboard,
+To move Right: press the "RIGHT" ARROW" key on the keyboard,
+To move Up: press the "UP ARROW" key on the keyboard,
+To move Down: press the "DOWN" ARROW" key on the keyboard,
+
+To Restart: "Refresh" the page
+
+**_Rules of the game_**
+
+If the snake touches the wall on either side of the game-board, the game will be automatically "OVER" and the snake will disappear.
+
+**_Deployment link_**
+
+Instructions
+
+you can use the below link where I have deployed my project and does not need any log in.
+
+https://github.com/ParulSingh16/ga-project-1
+
+Getting Started/Code Installation
+
+Instructions
+
+- follow the link above to get started
+- click the "scripts" folder to access the JavaScript code for the snake game
+- click the "styles" folder to acess the CSS used for the snake game
+- click the "index.html" to access the html code for the snake game
+- click the "images" folder to acess the images that has been used for the snake game
+
+- to play the game => open index.html and go live in the browser by clicking on "Port:5500" at the bottom right of your Vscode and then click "Go Live" this will open the snake game in the browser and you are ready to play.
+
+**Getting Started/Code Installation:**
+index.HTML
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles/main.css" />
+    <script src="./scripts/snake.js" charset="utf-8"></script>
+    <title>Snake</title>
+  </head>
+
+  <body>
+    <p>Current Score <span id="score-display">0</span></p>
+    <p>Press "Enter" to start</p>
+    <div>
+      <h1 data-text="SNAKE GAME">SNAKE GAME</h1>
+    </div>
+    <div class="game-board"></div>
+  </body>
+</html>
+
+**Timeframe, Brief & Working Team**
+
+Instructions
+This is the first individual project assigned by our Instructor to us.
+
+This project was given as a Solo project but, the game rooms were created with the free access to be in touch to help eachother working on the same game and thus I have been in the same game group with my two other very helpful classmates Ms. Alice Lo and Mr. Joel Sahiti.
+Also, we have been given the opportunity to seek help directly from our wonderful instructor- Mr. Tristan Hall and TA- Mr. Joe Freeman who have been really kind in helping us out solving the issues we have come across during this project.
+
+**Timeframe**
+We were given a week to complete this project.
+We were also guided on how we can divide our timeframe
+for eg: the project was assigned on Friday and we were suggested to get the MVP done by Monday evening or Tuesday morning and rest of the week we can then continue working on the CSS and Readme which actually helped us in understanding that a product that doesnt work is of no use if looks good. Thus, the most important task is to have a working game.
+
+**Technologies Used**
+Development Tools, Google, Google Fonts, youtube, VSCode, Git Hub repo.
--- scripts/snake.js
+++ scripts/snake.js
@@ -2,12 +2,20 @@
 document.addEventListener("DOMContentLoaded", createGrid);
 
 //define default variables
+const up = 38;
+const down = 40;
+const left = 37;
+const right = 39;
+const enter = 13;
 let gameBoard;
-const snakeBody = [0, 1, 2, 3, 4, 5];
-const speed = 2;
+const snakeBody = [0, 1];
+let speed = 2;
 let foodPosition = 0;
 let direction = "Right";
 let timer;
+let isOver = false;
+const scoreDisplay = document.querySelector("#score-display");
+let score = 0;
 
 function createGrid() {
   gameBoard = document.querySelector(".game-board");
@@ -16,28 +24,67 @@ function createGrid() {
     cell.setAttribute("class", "grid-cell");
     gameBoard.appendChild(cell);
   }
-  gameBoard.children[0].classList.add("snake-body");
+  updateSnakeOnScreen();
   generateFood();
 }
-
-function generateFood() {
-  while (gameBoard.children[foodPosition].classList.contains("snake-body")) {
-    foodPosition = Math.floor(Math.random() * 100);
-  }
-  gameBoard.children[foodPosition].classList.add("food");
+function scoreBoard() {
+  score = score + 1;
+  scoreDisplay.textContent = score;
 }
 
-document.addEventListener("keydown", handleKeyPress);
+// ****** FOOD RELATED FUNCTIONS ******
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+// generateFood will recursively generate a number, check if the number is
+// part of the snake body, and if not, it will place food
+function generateFood() {
+  const randomNumber = getRandomNumber();
+  if (gameBoard.children[randomNumber].classList.contains("snake-body")) {
+    generateFood();
+  } else {
+    foodPosition = randomNumber;
+    gameBoard.children[foodPosition].classList.add("food");
+  }
+}
+
+// feedSnake will remove the food and call generateFood function above
+function feedSnake() {
+  gameBoard.children[foodPosition].classList.remove("food");
+  generateFood();
+  scoreBoard();
+}
+
+// checkForFoodColision is called every time the snake moves
+function checkForFoodCollision() {
+  if (foodPosition === snakeBody[snakeBody.length - 1]) {
+    feedSnake();
+    scoreDisplay.textContent = score;
+    speed = speed + 1;
+  }
+}
+
+function startGame() {
+  timer = setInterval(moveSnake, 1000 / speed);
+}
+
+function moveSnake() {
+  removeSnake();
+  updateSnakeBody();
+  checkForCollision();
+  checkForFoodCollision();
+  if (!isOver) {
+    updateSnakeOnScreen();
+  }
+}
 
 function handleKeyPress(e) {
-  // Enter key is pressed
-  if (e.keyCode === 13) {
+  // Enter key is pressed either start the game or set the direction
+  if (e.keyCode === enter) {
     startGame();
   }
-  const up = 38;
-  const down = 40;
-  const left = 37;
-  const right = 39;
   if (e.keyCode === left && direction !== "Right") {
     direction = "Left";
   }
@@ -52,53 +99,85 @@ function handleKeyPress(e) {
   }
 }
 
-function startGame() {
-  timer = setInterval(moveSnake, 1000 / speed);
-}
+document.addEventListener("keydown", handleKeyPress);
 
-function moveSnake() {
+// removeSnake is called every time the snake moves
+function removeSnake() {
   snakeBody.forEach((cell) => {
     gameBoard.children[cell].classList.remove("snake-body");
   });
-  const head = snakeBody[snakeBody.length - 1]; //this is the head of the snake
-  if (direction === "Right") {
-    snakeBody.splice(0, 1); //removes the first element from the array(removes position 0)
-    snakeBody.push(head + 1);
-    //we are checking which direction snake is moving
-  }
-  if (direction === "Left") {
-    snakeBody.splice(0, 1); //removes the first element from the array(removes position 0)
-    snakeBody.push(head - 1);
-  }
-  if (direction === "Up") {
-    snakeBody.splice(0, 1); //removes the first element from the array(removes position 0)
-    snakeBody.push(head - 10);
-  }
-  if (direction === "Down") {
-    snakeBody.splice(0, 1); //removes the first element from the array(removes position 0)
-    snakeBody.push(head + 10);
-  }
-
-  updateSnakeOnScreen();
 }
 
+// remove tail will only be called if the snake DOES NOT eat the food
+function removeTail() {
+  snakeBody.splice(0, 1);
+}
+
+// updateSnakeBody will change the snakeBody array by changing the
+// head and tail depending on the direction of travel
+function updateSnakeBody() {
+  const head = snakeBody[snakeBody.length - 1];
+  if (direction === "Right") {
+    const newHeadPosition = head + 1;
+    if (newHeadPosition !== foodPosition) {
+      removeTail();
+    }
+    snakeBody.push(newHeadPosition);
+  }
+  if (direction === "Left") {
+    const newHeadPosition = head - 1;
+    if (newHeadPosition !== foodPosition) {
+      removeTail();
+    }
+    snakeBody.push(newHeadPosition);
+  }
+  if (direction === "Up") {
+    const newHeadPosition = head - 10;
+    if (newHeadPosition !== foodPosition) {
+      removeTail();
+    }
+    snakeBody.push(newHeadPosition);
+  }
+  if (direction === "Down") {
+    const newHeadPosition = head + 10;
+    if (newHeadPosition !== foodPosition) {
+      removeTail();
+    }
+    snakeBody.push(newHeadPosition);
+  }
+}
+
+// checkForCollision is called every time the updateSnakeBody changes the snakeBody array
+function checkForCollision() {
+  // checks for colision and ends game if snake passes through a wall
+  const head = snakeBody[snakeBody.length - 1];
+  const body = snakeBody[snakeBody.length - 2];
+  const rowThatHeadIsIn = Math.floor(snakeBody[snakeBody.length - 1] / 10);
+
+  const headPosition = head % 10;
+  const bodyPosition = body % 10;
+
+  const headIsInAWall = headPosition === 9 || headPosition === 0;
+  const bodyIsInAWall = bodyPosition === 9 || bodyPosition === 0;
+  if (
+    headIsInAWall &&
+    bodyIsInAWall &&
+    (direction === "Right" || direction === "Left")
+  ) {
+    isOver = true;
+    clearInterval(timer);
+  } else if (
+    (rowThatHeadIsIn < 0 || rowThatHeadIsIn > 9) &&
+    (direction === "Down" || direction === "Up")
+  ) {
+    isOver = true;
+    clearInterval(timer);
+  }
+}
+
+// updateSnakeonScreen is called every time we move the snake
 function updateSnakeOnScreen() {
   snakeBody.forEach((cell) => {
     gameBoard.children[cell].classList.add("snake-body");
   });
-  const head = snakeBody[snakeBody.length - 1];
-  const x = head % 10;
-  const rightHandwall = x === 9;
-  console.log(rightHandwall, direction);
-  const leftHandwall = x === 0; // if the head is going to be in or not
-  if (rightHandwall && direction === "Right") {
-    alert("Game Over!!");
-  }
-
-  if (foodPosition === head) {
-    gameBoard.children[foodPosition].classList.remove("food");
-    //if snake eats the food, put the tail back to expand the snake
-    //snakeBody.unshift(tailObject);
-  }
-  generateFood();
 }
